0bb0c9c06ed5ae61e477744ecbb4f16f10394eb6
Vlad <50303876+tw9ntyseven@users.noreply.github.com>
2020-01-29 00:21:56 +0300

    some gulp

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,30 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
-    plugins: [ 
-        new webpack.ProvidePlugin({ 
-            "jQuery": "jquery", 
-            "window.jQuery": "jquery", 
-            "jquery": "jquery", 
-            "window.jquery": "jquery", 
-            "$": "jquery", 
-            "window.$": "jquery" 
-        }) 
-    ],
-    
-    entry: {
-        main: "./src/js/index.js",
-    },
+	output: {
+		filename: "[name].js"
+	},
 
-    output: {
-        filename: "[name].js",
-        chunkFilename: "[name].js",
-        publicPath: "/"
-    },
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: require.resolve("babel-loader"),
+					query: {
+						presets: [
+							["@babel/preset-env", { modules: false }]
+						]
+					}
+				}
+			}
+		]
+	},
 
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /node_modules/,
-                    chunks: "initial",
-                    name: "vendor",
-                    enforce: true
-                }
-            }
-        }
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: require.resolve("babel-loader"),
-                    query: {
-                        presets: [
-                            ["@babel/preset-env", { modules: false }]
-                        ]
-                    }
-                }
-            }
-        ]
-    },
-
-    resolve: {
-        alias: {
-            "%modules%": path.resolve(__dirname, "src/blocks/modules")
-        }
-    }
+	resolve: {
+		alias: {
+			"%blocks%": path.resolve(__dirname, "src/blocks")
+		}
+	}
 };
