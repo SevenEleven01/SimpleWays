99a86ac0be71176224f5ddfc1b71ffc072f02213
tw9ntyseven <50303876+tw9ntyseven@users.noreply.github.com>
2020-02-03 14:41:43 +0300

    add branch reg and complete form reg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Sign from './pages/sign/sign'
+import Sign from './pages/sign/sign';
+import Reg from './pages/reg/reg';
 import { render } from '@testing-library/react';
 
 function App() {
   return (
     <div className="wrapper">
-      <Sign />
+      {/* <Sign /> */}
+      <Reg />
     </div>
   );
 }
