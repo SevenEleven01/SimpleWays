f36641456faf480c6e3134a0036932af12f3f306
tw9ntyseven <50303876+tw9ntyseven@users.noreply.github.com>
2020-02-27 20:58:22 +0300

    add form for Sign In

--- src/pages/sign/sign.js
+++ src/pages/sign/sign.js
@@ -11,11 +11,33 @@ import '../../components/input-password.css';
 class sign extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: "Регистрация" };
+    this.state = { 
+      text: "Регистрация",
+      email: '',
+      password: ''
+    };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+}
+handleSubmit(event) {
+  event.preventDefault();
+  console.log('form is submited. value is', this.state.email, this.state.password);
+}
+handleEmailChange(event) {
+  console.log('email was changed', event.target.value);
+  this.setState({email: event.target.value});
+}
+handlePasswordChange(event) {
+  console.log('pass was changed', event.target.value);
+  this.setState({password: event.target.value});
 }
   render() {
     return (
       <div className="wrapper">
+
+        {/* This NavBar for work sign and reg */}
+
         <>
             <Menu className="menu">
             <Link to="/reg" className="header-bar_button" >
@@ -39,18 +61,41 @@ class sign extends Component {
             <Link to="/" className="header-bar_logo">SimpleWays</Link>
             </div>
         </>
+
+        {/* This page */}
+
      <div className="sign-wrapper">
        <div className="sign">
         <div className="sign-form">
           <h2 className="sign-form_title">Вход</h2>
           <p className="sign-form_subtitle">Используйте аккаунт SimpleWays</p>
-          <input className="login" type="login" name="login" placeholder="Введите адрес эл. почты" />
-          <input className="password" type="password" name="password" placeholder="Введите пароль"/>
+
+          {/* Form sign in */}
+          
+          <form onSubmit={this.handleSubmit}>
+            <input 
+              className="login" 
+              type="login" 
+              name="login" 
+              placeholder="Введите адрес эл. почты" 
+              value={this.state.email}
+              onChange={this.handleEmailChange}
+            />
+          <input 
+            className="password" 
+            type="password" 
+            name="password" 
+            placeholder="Введите пароль"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+          </form>
+
           <div className="sign-form_buttons">
             <Link to="/reg" className="sign-form_add">
               Создать аккаунт
               </Link>
-          <button className="sign-button button">Вход</button>
+          <button onClick={this.handleSubmit} className="sign-button button">Вход</button>
           </div>
         </div>
        </div>
