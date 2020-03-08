aab7fbc6bcb4acd812ac1c86a910708d96844294
tw9ntyseven <vladakmilicin@mail.ru>
2020-03-08 13:15:49 +0300

    add form in ways

--- src/pages/ways/ways.js
+++ src/pages/ways/ways.js
@@ -4,13 +4,37 @@ import './ways.css';
 import '../../components/button/button.css';
 
 export class ways extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numWay: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handlenumWay = this.handlenumWay.bind(this);
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('form is submited. value is', this.state.numWay);
+      }
+      handlenumWay(event) {
+        console.log('numWay was changed', event.target.value);
+        this.setState({numWay: event.target.value});
+      }
     render() {
         return (
             <div className="ways-wrapper">
                 <HeadBar />
                 <div className="ways">
                 <h1 className="ways-title">Маршруты</h1>
-                <input className="login ways-input" type="number" name="ways" placeholder="Введите номер маршрута"/>
+                <form onSubmit={this.handleSubmit}>
+                <input
+                 className="login ways-input"
+                  type="number"
+                   name="ways"
+                    placeholder="Введите номер маршрута"
+                    value={this.state.numWay}
+                     onChange={this.handlenumWay}
+                    />
                 <button className="ways-button button"><i class="fas fa-search"></i></button>
                 <span className="ways-radio_block">
                     <input id="input1" className="ways-radio_button" type="radio" name="way" />
@@ -18,6 +42,7 @@ export class ways extends Component {
                     <input id="input2" className="ways-radio_button" type="radio" name="way" />
                     <label for="input2" className="ways-radio_block-item" >Название остановки</label>
                 </span>
+                </form>
                 </div>
             </div>
         );
